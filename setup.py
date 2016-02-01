c47a171bddcf6de02e814c9ba16271a320b25cf2
Mateusz Krakowski <Kraaaki01@gmail.com>
2016-02-01 20:49:26 +0100

    Update setup.py
    Just for testing, try installing package after uninstalling scipy and look what happens

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Information Analysis",
     ],
-    install_requires = ['numpy']
+    install_requires = ['numpy', 'scipy']
 )
