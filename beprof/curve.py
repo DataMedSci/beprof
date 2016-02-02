7e92e93b0f61ec57f7324465f539b3f72df643db
Mateusz Krakowski <Kraaaki01@gmail.com>
2016-02-02 20:14:15 +0100

    Update curve.py
    1) Added fixed_step_domain() method. I assumed the first solution of 2 I mentioned in issue #4 is correct. 
    2) Content of main() is just for testing. Once you say methods are implemented correctly I will remove those and write docstring tests instead.
    3) 'prints' inside methods - need to figure out what should be done in this place of code. User should know somehow that method is not working as it 
        is supposed to be. Exception seems to be a good idea here. What do you think?

--- beprof/curve.py
+++ beprof/curve.py
@@ -68,23 +68,38 @@ class Curve(np.ndarray):
 
     def change_domain(self, domain):
 
-        #check if new domain includes in the orginal domain
-        if np.max(domain)>np.max(self.x):
-            #maybe some exception here or stderr log ?
+        # check if new domain includes in the orginal domain
+        if np.max(domain) > np.max(self.x):
+            # maybe some exception here or stderr log ?
+            # these prints only for testing purpose
             print('Error1')
+            print('domain.max: ', np.max(domain), '|| self.max: ', np.max(self.x))
             return self
-        if np.min(domain)<np.min(self.x):
+        if np.min(domain) < np.min(self.x):
             print('Error2')
+            print('domain.min: ', np.min(domain), '|| self.min: ', np.min(self.x))
             return self
         y = np.interp(domain, self.x, self.y)
-        obj = Curve([[domain[ind], y[ind]] for ind in range(0, len(y)) ])
+        obj = Curve([[domain[ind], y[ind]] for ind in range(0, len(y))])
         return obj
+
+    def fixed_step_domain(self, step=0.1, fixp=0):
+        section = (np.min(self.x), np.max(self.x))
+        if fixp < section[0] or fixp > section[1]:
+            # some kind of error just as in change_domain above?
+            print("Section: ", section, "fixp: ", fixp)
+            print('Error3')
+            return self
+        count = int(np.min([fixp - section[0], section[1] - fixp]) / step)
+        domain = [fixp + n * step for n in range(-count, count+1)]
+        return self.change_domain(domain)
 
     def __str__(self):
         ret = "shape: {}".format(self.shape) + \
               " X : [{:4.3f},{:4.3f}]".format(min(self.x), max(self.x)) + \
               " Y : [{:4.6f},{:4.6f}]".format(min(self.y), max(self.y))
         return ret
+
 
 def main():
     c = Curve([[0, 0], [5, 5], [10, 0]])
@@ -93,10 +108,22 @@ def main():
     for x in (0.5, 1, 1.5, 2.0, 4.5):
         print("x=", x, "y=", c.y_at_x(x))
 
-    new = c.change_domain([1, 2, 3, 5, 6, 7, 9])
 
+    print('\n', '*'*30,'\nchange_domain:')
+
+    print("X:", c.x)
+    print("Y:", c.y)
+    new = c.change_domain([1, 2, 3, 5, 6, 7, 9])
     print("X:", new.x)
     print("Y:", new.y)
+
+    print('\n', '*'*30,'\nfixed_step_domain:')
+
+    print("X:", c.x)
+    print("Y:", c.y)
+    test = c.fixed_step_domain(0.4, 7)
+    print("X:", test.x)
+    print("Y:", test.y)
 
 if __name__ == '__main__':
     main()
