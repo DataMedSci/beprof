b104da3ffe0cd8ef00e28c9f0dc87a384db0d17f
Mateusz Krakowski <Kraaaki01@gmail.com>
2016-02-03 13:10:53 +0100

    Update curve.py
    Added metadata to Curve class, provided passing informations in change_domain function by using __dict__

--- beprof/curve.py
+++ beprof/curve.py
@@ -31,8 +31,12 @@ class Curve(np.ndarray):
     Has additional field - info.
     """
 
-    def __new__(cls, input_array):
+    def __new__(cls, input_array, **meta):
         obj = np.asarray(input_array).view(cls).copy()
+        if meta is None:
+            obj.metadata = {}
+        else:
+            obj.metadata = meta.copy()
         return obj
 
     def __array_finalize__(self, obj):
@@ -68,9 +72,43 @@ class Curve(np.ndarray):
 
     def __str__(self):
         ret = "shape: {}".format(self.shape) + \
-              " X : [{:4.3f},{:4.3f}]".format(min(self.x), max(self.x)) + \
-              " Y : [{:4.6f},{:4.6f}]".format(min(self.y), max(self.y))
+              "\nX : [{:4.3f},{:4.3f}]".format(min(self.x), max(self.x)) + \
+              "\nY : [{:4.6f},{:4.6f}]".format(min(self.y), max(self.y)) + \
+              "\nMetadata : " + str(self.metadata)
         return ret
+
+    # functions not ready yet, added for testing metadata purpose
+    def change_domain(self, domain):
+
+        # check if new domain includes in the orginal domain
+        if np.max(domain) > np.max(self.x):
+            # maybe some exception here or stderr log ?
+            # these prints only for testing purpose
+            print('Error1')
+            print('domain.max: ', np.max(domain), '|| self.max: ', np.max(self.x))
+            return self
+        if np.min(domain) < np.min(self.x):
+            print('Error2')
+            print('domain.min: ', np.min(domain), '|| self.min: ', np.min(self.x))
+            return self
+        y = np.interp(domain, self.x, self.y)
+        # Important **self.__dict__ argument
+        obj = Curve([[domain[ind], y[ind]] for ind in range(0, len(y))], **self.__dict__)
+        return obj
+
+    def fixed_step_domain(self, step=0.1, fixp=0):
+        section = (np.min(self.x), np.max(self.x))
+        if fixp < section[0] or fixp > section[1]:
+            # some kind of error just as in change_domain above?
+            print("Section: ", section, "fixp: ", fixp)
+            print('Error3')
+            return self
+        count = int(np.min([fixp - section[0], section[1] - fixp]) / step)
+        domain = [fixp + n * step for n in range(-count, count+1)]
+        return self.change_domain(domain)
+
+    ###########################
+
 
 def main():
     c = Curve([[1, 0], [2, 1], [4, 0]])
@@ -78,6 +116,23 @@ def main():
     print("Y:", c.y)
     for x in (0.5, 1, 1.5, 2.0, 4.5):
         print("x=", x, "y=", c.y_at_x(x))
+
+    print('\n', '*'*30, 'Metadata testing\n')
+
+    k = Curve([[0, 1], [1, 2], [2, 3], [4, 0]], now='I', got='it')
+    print('X:', k.x)
+    print('Y:', k.y)
+    print('M:', k.metadata)
+
+    print(k)
+
+    print('\nTEST:')
+    test = k.fixed_step_domain(0.5, 3)
+    print('X:', test.x)
+    print('Y:', test.y)
+    print('M:', test.metadata)
+
+    print(test)
 
 
 if __name__ == '__main__':
