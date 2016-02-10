a4250662be8bfa35bcd6a8a6c0277ccb3cddb305
Mateusz Krakowski <Kraaaki01@gmail.com>
2016-02-10 17:41:08 +0100

    Fixed some terrible solutions
    Now its better, but still not perfect.

--- beprof/functions.py
+++ beprof/functions.py
@@ -1,5 +1,5 @@
-import beprof.curve
-import beprof.profile
+import curve
+import profile
 import numpy as np
 
 def subtract(curve1, curve2, defval=0):
@@ -35,10 +35,8 @@ def subtract(curve1, curve2, defval=0):
     #     return obj
     # # CASE 2: one domain includes the other, in this example domain(curve2) is inside domain(curve1)
     # elif a1 <= a2 and b1 >= b2:
-    set1 = set(curve1.x)
-    set2 = set(curve2.x)
-    newX = list(set1 | set2)
-    newX.sort()
+
+    newX = np.union1d(curve1.x, curve2.x)
     print('newX: ', newX)
     y1 = curve1.evaluate_at_x(newX, defval)
     y2 = curve2.evaluate_at_x(newX, defval)
