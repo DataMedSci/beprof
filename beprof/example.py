85d7343419410172c4a589148a270707e071f5df
Mateusz Krakowski <Kraaaki01@gmail.com>
2016-02-12 12:00:47 +0100

    Sorry
    I must agree, that reading manual can be helpful.

--- beprof/example.py
+++ beprof/example.py
@@ -3,13 +3,11 @@ from beprof.curve import Curve
 import numpy as np
 
 url = 'http://berkeleyearth.lbl.gov/auto/Global/Complete_TAVG_complete.txt'
-data = urllib.request.urlopen(url)
 
-all = np.genfromtxt(data, skip_header=35, skip_footer=2192)
-print(np.shape(all))
-x = np.array(range(0, 1000))
-y = all[:,3]
+y = np.genfromtxt(urllib.request.urlopen(url), skip_header=35, skip_footer=2192, usecols=3)
+x = np.arange(0, 1000)
 
 example = Curve(np.stack((x, y), axis=1))
 print(example)
+
 
