626999691a0df1ebef40e372ceaf06eccfb28ecd
Mateusz Krakowski <Kraaaki01@gmail.com>
2016-02-18 16:04:45 +0100

    Profiles tests added
    Working on Exceptions right now, as mentioned in the comment merged 18-cleaning methods and subtracting curves 
    create new test possibilities

--- beprof/unittests.py
+++ beprof/unittests.py
@@ -1,14 +1,21 @@
-from curve import Curve, DataSerie
-from profile import Profile, LateralProfile
+from beprof.curve import Curve, DataSerie
+from beprof.profile import Profile, LateralProfile
 import numpy as np
 import unittest
+import copy
 
 cur = Curve([[0, 0], [1, 1], [2, 2], [3, 2], [4, 3], [5, 4], [6, 4], [7, 3], [8, 2], [9, 1], [10, 0]],
             arg1 = 'first',
             arg2 = 'second'
             )
-pro = Profile([[0, 0], [1, 2]])
-lpr = LateralProfile([[0, 0], [1, 2]])
+pro = Profile([[0, 0], [1, 1], [2, 2], [3, 2], [4, 3], [5, 4], [6, 4], [7, 3], [8, 2], [9, 1], [10, 0]],
+            profile1 = 'first_prof',
+            profile2 = 'second_prof'
+            )
+lpr = LateralProfile([[0, 0], [1, 1], [2, 2], [3, 2], [4, 3], [5, 4], [6, 4], [7, 3], [8, 2], [9, 1], [10, 0]],
+            lprof1 = 'first_lp',
+            lprof2 = 'second_lp'
+            )
 
 
 class TestCurveMethods(unittest.TestCase):
@@ -48,6 +55,61 @@ class TestCurveMethods(unittest.TestCase):
         self.assertEqual(list(cur.rebinned(step=1, fixp=0.5).x),
                          [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5]
                          )
+class TestProfileMethods(unittest.TestCase):
+
+    def test_metadata_1(self):
+        # check if everything was OK with constructing object with metadata
+        self.assertEqual(pro.metadata['profile1'], 'first_prof')
+        self.assertEqual(pro.metadata['profile2'], 'second_prof')
+        # check if object after change_domain still has metadata
+        obj = pro.change_domain([0, 1, 2.5, 5])
+        self.assertEqual(obj.metadata['profile1'], 'first_prof')
+        self.assertEqual(obj.metadata['profile2'], 'second_prof')
+        # try creating new obj with slicing and check metadata
+        obj = pro[2:5]
+        self.assertEqual(obj.metadata['profile1'], 'first_prof')
+        self.assertEqual(obj.metadata['profile2'], 'second_prof')
+        # try get non existing metadata
+        with self.assertRaises(KeyError):
+            pro.metadata['example']
+            obj.metadata['non-existing']
+
+    def test_x_at_y(self):
+        self.assertEqual(pro.x_at_y(1), 1)
+        self.assertEqual(pro.x_at_y(1, reverse=True), 9)
+
+        # y value out of range
+        self.assertTrue(np.isnan(pro.x_at_y(5)))
+
+    def test_mirror(self):
+        # mirroring twice should be like non mirroring at all
+        orgx = copy.deepcopy(lpr.x)
+        orgy = copy.deepcopy(lpr.y)
+        lpr.mirror()
+        lpr.mirror()
+        self.assertEqual(list(lpr.x), list(orgx))
+        self.assertEqual(list(lpr.y), list(orgy))
+        # simple example
+        lpr.mirror(m=4)
+        self.assertEqual(list(lpr.y), list(orgy[::-1]))
+
+class TestTypes(unittest.TestCase):
+
+    # testing different combinations of profiles using curve methods
+    # check whether type of returned object and metadata is OK
+    # need more up-to-date version of repo, all tests will fail on this level
+    def test_rebinning(self):
+        # todo
+        pass
+
+    def test_change_domain(self):
+        # todo
+        pass
+
+class TestExceptions(unittest.TestCase):
+    def test_exceptions(self):
+        # todo
+        pass
 
 if __name__ == '__main__':
     unittest.main()
