46f34b55e6a5cb1c045e9a222b9aee9b4ce1c242
ant6 <ant6ant@gmail.com>
2016-05-30 02:48:04 +0200

    Test stubs #19

---
+++ beprof/test/test_curve.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+
+
+class TestCurve(TestCase):
+    def test_x(self):
+        self.fail()
+
+    def test_x(self):
+        self.fail()
+
+    def test_y(self):
+        self.fail()
+
+    def test_y(self):
+        self.fail()
+
+    def test_rescale(self):
+        self.fail()
+
+    def test_smooth(self):
+        self.fail()
+
+    def test_y_at_x(self):
+        self.fail()
+
+    def test_change_domain(self):
+        self.fail()
+
+    def test_rebinned(self):
+        self.fail()
+
+    def test_evaluate_at_x(self):
+        self.fail()
+
+    def test_subtract(self):
+        self.fail()
---
+++ beprof/test/test_profile.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestProfile(TestCase):
+    def test_x_at_y(self):
+        self.fail()
+
+    def test_width(self):
+        self.fail()
+
+    def test_fwhm(self):
+        self.fail()
+
+    def test_normalize(self):
+        self.fail()
