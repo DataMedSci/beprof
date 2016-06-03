3daec9884457c76e177ba45b592de13d2b4ab9ea
ant6 <ant6@users.noreply.github.com>
2016-06-03 12:26:17 +0200

    Unit tests #19 (#36)
    * Test stubs #19
    
    * Some travis files for testing :zap:
    
    * Super-simple test to check travis
    
    * travis py3.2 fix
    
    * Added small test for Curve.smooth

---
+++ beprof/test/test_profile.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestProfile(TestCase):
+    def test_x_at_y(self):
+        pass
+
+    def test_width(self):
+        pass
+
+    def test_fwhm(self):
+        pass
+
+    def test_normalize(self):
+        pass
