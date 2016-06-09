c7d37e2226ced7df03294ab8b2c75440cd51e397
Leszek Grzanka <Leszek.Grzanka@ifj.edu.pl>
2016-06-09 10:58:54 +0200

    versioneer and GIT - PoC

--- setup.py
+++ setup.py
@@ -1,19 +1,58 @@
 import setuptools
 from pkg_resources import parse_version
+
+
+def pip_command_output(pip_args):
+    import sys
+    import pip
+    from io import StringIO
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    pip.main(pip_args)
+    output = mystdout.getvalue()
+    mystdout.truncate(0)
+    sys.stdout = old_stdout
+    print("result", pip_args, len(output), output)
+    return output
+
+
 try:
+    # assume versioneer.py was generated using "versioneer install" command
     import versioneer
 except ImportError:
-    # dirty hack needed by readthedoc generation tool
+    # it looks versioneer.py is missing
+    # lets assume that versioneer package is installed
+    # and versioneer binary is present in $PATH
     import subprocess
-    subprocess.call(["versioneer", "install"])
-    import versioneer
+    try:
+        # call versioneer install to generate versioneer.py
+        subprocess.call(["versioneer", "install"])
+    except IOError:
+        # it looks versioneer is missing from $PATH
+        # probably versioneer is installed in some user directory
+
+        # query pip for list of files in versioneer package
+        output = pip_command_output(["show", "-f", "versioneer"])
+
+        # now we parse the results
+        main_path = [x[len("Location: "):] for x in output.split('\n')
+                     if x.startswith("Location")][0]
+        bin_path = [x[len("  "):] for x in output.split('\n')
+                    if x.endswith("/versioneer")][0]
+
+        # exe_path is absolute path to versioneer binary
+        import os
+        exe_path = os.path.join(main_path, bin_path)
+        # call versioneer install to generate versioneer.py
+        subprocess.call([exe_path, "install"])
+
+        import versioneer
 
 version = versioneer.get_version()
 parsed_version = parse_version(version)
 if '*@' in parsed_version[1]:
     import time
     version += str(int(time.time()))
-
 
 packages = ['beprof']
 
@@ -26,7 +65,7 @@ setuptools.setup(
     packages=packages,
     url='https://github.com/DataMedSci/beprof',
     license='GPL',
-    author=['Leszek Grzanka','Mateusz Krakowski', 'ant6'],
+    author=['Leszek Grzanka', 'Mateusz Krakowski', 'ant6'],
     author_email='grzanka@agh.edu.pl',
     description='Beam Profile Analysing Tools',
     long_description=readme + '\n',
