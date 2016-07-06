8ffeb740b73ac82c55eec3ff02f21ade5df1a132
ant6 <ant6ant@gmail.com>
2016-07-06 19:40:44 +0200

    Better tests for Curve rescale and smooth

--- tests/unit_test/test_curve.py
+++ tests/unit_test/test_curve.py
@@ -1,22 +1,46 @@
-from unittest import TestCase
-
+import pytest
+import unittest
 from beprof.curve import Curve
 
 
-class TestCurve(TestCase):
+class TestCurve(unittest.TestCase):
     def setUp(self):
         self.pre = Curve([[0.0, 0], [5, 5], [10, 0]])
         self.post = Curve([[0.0, 0], [5, 5], [10, 0]])
 
+    # TEST RESCALE
     def test_rescale_by_one(self):
-        self.post.rescale(1)
-        self.assertEqual(list(self.pre.x), list(self.post.x))
-        self.assertEqual(list(self.pre.y), list(self.post.y))
+        self.post.rescale(factor=1)
+        assert list(self.pre.x) == list(self.post.x)
+        assert list(self.pre.y) == list(self.post.y)
 
+    def test_rescale_by_negative_one(self):
+        self.post.rescale(factor=-1)
+        assert list(self.pre.x) == list(self.post.x)
+        assert list(self.pre.y) == list(self.post.y * -1)
+
+    def test_rescale_by_zero(self):
+        # post.y should contain some nans and infs
+        self.post.rescale(factor=0)
+        assert list(self.pre.x) == list(self.post.x)
+        assert list(self.pre.y) != list(self.post.y)
+        assert str(self.post.y[0]) == 'nan'
+        assert str(self.post.y[1]) == 'inf'
+
+    # TEST SMOOTH
     def test_smooth_with_window_one(self):
         self.post.smooth(window=1)
-        self.assertEqual(list(self.pre.x), list(self.post.x))
-        self.assertEqual(list(self.pre.y), list(self.post.y))
+        assert list(self.pre.x) == list(self.post.x)
+        assert list(self.pre.y) == list(self.post.y)
+
+    def test_smooth_with_negative_window(self):
+        with pytest.raises(ValueError):
+            self.post.smooth(window=-1)
+
+    def test_smooth_wtih_window_zero(self):
+        with pytest.raises(ValueError):
+            self.post.smooth(window=0)
+
     #
     # def test_y_at_x(self):
     #     self.fail()
