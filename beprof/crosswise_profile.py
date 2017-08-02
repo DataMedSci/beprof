ddce7e1ddacab8f09e000aa56c6437a08c73bee0
MarekOchocki <marcopolo97@vp.pl>
2017-08-02 16:14:19 +0200

    new class

--- beprof/crosswise_profile.py
+++ beprof/crosswise_profile.py
@@ -1,1 +1,9 @@
+from beprof import profile
+from beprof import curve
+import numpy as np
 
+
+class CrosswiseProfile(profile.Profile):
+    def normalize(self):
+        #nothing done yet
+        return 7
